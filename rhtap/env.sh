408c2a288ff0c019d84a18c93c4584dc55dd58e8
Josef Kopriva <jkopriva@redhat.com>
2024-11-05 13:15:21 +0100

    Update TUF mirror in environment file

--- rhtap/env.sh
+++ rhtap/env.sh
@@ -47,4 +47,4 @@ export INFO=${INFO-true}
 export STRICT=${STRICT-true}
 export EFFECTIVE_TIME=${EFFECTIVE_TIME-now}
 export HOMEDIR=${HOMEDIR-$(pwd)}
-export TUF_MIRROR=${TUF_MIRROR-http://tuf.rhtap-tas.svc}
+export TUF_MIRROR=${TUF_MIRROR-https://tuf-rhtap-tas.apps.rosa.xfp28-jztd2-u9j.groh.p3.openshiftapps.com}
